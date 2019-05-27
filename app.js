8018a13d88bfece92cd3ee172e83351b28dbb80b
Daniel Wang <pi@officialdaniel.com>
2019-05-27 18:05:30 +0000

    revert https config

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const express = require('express');
-const fs = require('fs');
-const https = require('https')
 const app = express();
 const port = 8080;
 
 app.use(express.static('home'));
 
-https.createServer({
-    key: fs.readFileSync('server.key'),
-    cert: fs.readFileSync('server.cert')
-}, app).listen(port, function () {
-        console.log('Example app listening on port 3000! Go to https://localhost:8080/')
-    });
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
