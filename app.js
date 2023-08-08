29961d77e67b4eb6d7ae32e9685b6ceeada9adc4
Chenguang Wang <daniel@Chenguangs-MBP.home>
2023-08-08 16:16:32 -0700

    add ip info finder

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const ipInfo = require("ip-info-finder");
 
 app.use(express.static('home'));
 
 // Middleware to log the client IP
 app.use((req, res, next) => {
     console.log(`Request from IP: ${req.ip}`);
+    const realIP = req.ip.split(':')[req.ip.split(':').length - 1];
+    ipInfo.getIPInfo(
+        realIP
+    ).then(data => {
+        console.log("find ip info!!!!")
+        console.log(data);
+    })
+        .catch(err => console.log(err));
     next();
 });
 
