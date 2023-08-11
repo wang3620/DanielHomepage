031f77420ef7a22b988c9e781200ccaf3eb4df70
Daniel Wang <chenguang89.wang@outlook.com>
2023-08-11 13:44:43 -0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
-const db = require('./db')
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user:'daniel',
+    password: '7777',
+    database: "danielHomepage",
+    connectionLimit: 5
+});
 const app = express();
 const port = 8080;
 
@@ -8,20 +15,33 @@ const ipInfo = require("ipinfo")
 app.use(express.static('home'));
 
 // Middleware to log the client IP
-app.use((req, res, next) => {
-    console.log(`Request from IP: ${req.ip}`);
-    const realIP = req.ip.split(':')[req.ip.split(':').length - 1];
-    db.pool.query("select * from ip_location_history;").then(res => {
-        console.log("get db query response");
+app.use(async (req, res, next) => {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        console.log(`Request from IP: ${req.ip}`);
+        const realIP = req.ip.split(':')[req.ip.split(':').length - 1];
+        const realIpInfo = await ipInfo(realIP);
+        const location = {
+            longitude: null,
+            latitude: null,
+        }
+        if (realIpInfo.loc) {
+            const arr = realIpInfo.loc.split(",");
+            const log = arr[0];
+            const lat = arr[1];
+            location.longitude = log;
+            location.latitude = lat;
+        }
+        const res = await conn.query("INSERT INTO ip_location_history (location) values (?)", [location]);
         console.log(res);
-    }).catch(err => {
-        console.log("get db query err");
+
+    } catch (err) {
+        console.log("get err");
         console.log(err);
-    })
-    ipInfo(realIP, (err, cLoc) => {
-        console.log(err || cLoc)
-    })
-    next();
+    } finally {
+        next();
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/Mapbox.jsx
+++ src/Mapbox.jsx
@@ -4,6 +4,15 @@ import maplibregl from 'maplibre-gl';
 import DeckGL from '@deck.gl/react';
 import {HeatmapLayer} from '@deck.gl/aggregation-layers';
 
+const mariadb = require('mariadb');
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user:'daniel',
+    password: '7777',
+    database: "danielHomepage",
+    connectionLimit: 5
+});
+
 const DATA_URL =
     'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/screen-grid/uber-pickup-locations.json'; // eslint-disable-line
 
@@ -18,29 +27,37 @@ const INITIAL_VIEW_STATE = {
 
 const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
 
-export default function Mapbox({
-                                data = DATA_URL,
+export default async function Mapbox({
+                                         data = DATA_URL,
                                 intensity = 1,
                                 threshold = 0.03,
                                 radiusPixels = 30,
                                 mapStyle = MAP_STYLE
                             }) {
-    const layers = [
-        new HeatmapLayer({
-            data,
-            id: 'heatmp-layer',
-            pickable: false,
-            getPosition: d => [d[0], d[1]],
-            getWeight: d => d[2],
-            radiusPixels,
-            intensity,
-            threshold
-        })
-    ];
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        const res = await conn.query("select * from ip_location_history");
+        console.log(res);
+        const layers = [
+            new HeatmapLayer({
+                data,
+                id: 'heatmp-layer',
+                pickable: false,
+                getPosition: d => [d[0], d[1]],
+                getWeight: d => d[2],
+                radiusPixels,
+                intensity,
+                threshold
+            })
+        ];
 
-    return (
-        <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={layers}>
-            <Map reuseMaps mapLib={maplibregl} mapStyle={mapStyle} preventStyleDiffing={true} />
-        </DeckGL>
-    );
+        return (
+            <DeckGL initialViewState={INITIAL_VIEW_STATE} controller={true} layers={layers}>
+                <Map reuseMaps mapLib={maplibregl} mapStyle={mapStyle} preventStyleDiffing={true} />
+            </DeckGL>
+        );
+    } catch (err) {
+        return <div>error in init</div>
+    }
 }
